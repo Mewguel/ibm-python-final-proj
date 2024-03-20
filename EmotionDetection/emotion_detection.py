bc16e948b1cf30c891ee50c2098a5de42b1ae38f
Ivan Miguel Malabanan <meguelvan@gmail.com>
2024-03-20 05:23:21 -0400

    [Complted] Task 1-6

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,57 @@
+''' Contains the emotion analyzer using watson sentiment api
+'''
+import json
+import requests
+
+
+def emotion_detector(text_to_analyze):
+    '''
+    This function performs emotion detection on the given text using the Watson Emotion Detection API.
+    Args:
+        text_to_analyze (str): The text to be analyzed for emotions. 
+    Returns:
+        str: The text attribute of the response object received from the Emotion Detection API.
+    
+    '''
+    url = ('https://sn-watson-emotion.labs.skills.network/'
+           'v1/watson.runtime.nlp.v1/NlpService/EmotionPredict')
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+
+    try:
+        response = requests.post(url, json=myobj, headers=headers, timeout=10)
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            formatted_response = json.loads(response.text)
+            emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
+
+            dominant_emotion = ''
+            dominant_score = 0
+            for emotion, score in emotion_dict.items():
+                if score > dominant_score:
+                    dominant_score = score
+                    dominant_emotion = emotion
+
+            emotion_dict['dominant_emotion'] = dominant_emotion
+            return emotion_dict
+
+        elif response.status_code == 400:
+            emotion_dict = {
+                'anger': None,
+                'disgust': None,
+                'fear': None,
+                'joy': None,
+                'sadness': None,
+                'dominant_emotion': None
+            }
+            return emotion_dict
+            
+        elif response.status_code == 500:
+            return None      
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+if __name__ == '__main__':
+    print(emotion_detector("I am so happy I am doing this."))
